a66b2d81e36e378d9b98aa2afe386075756d225f
Showto <marsegovia@frba.utn.edu.ar>
2023-11-25 12:14:16 -0300

    tire un par de codes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import '../css/Footer.css'
+
+function Footer(){
+    return(
+        <>
+            <footer>
+                <ul>
+                    <li>About</li>
+                    <li>Politicas</li>
+                    <li>Contacto</li>
+                </ul>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import '../css/Header.css'
+
+function Header(){
+    return(
+        <>
+        <header>
+            <img src="" alt="Logo de la pagina" />
+            <ul>
+                <a href=""><li>Iniciar sesion</li></a>
+                <a href=""><li>Cuenta</li></a>
+                <a href=""><li>Carrito</li></a>
+            </ul>
+        </header>
+        </>
+    )
+}
+
+export default Header
