e10323a7359ad1b489f13f7ff939613ed24846d1
Showto <marsegovia@frba.utn.edu.ar>
2023-11-25 15:38:55 -0300

    base de la pagina?

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Header from './components/Header'
 import './App.css'
+import Header from './components/Header'
 import Footer from './components/Footer'
+import Inicio from './components/Inicio'
 
 export default function App(){
   return(
     <>
     <Header/>
+
+    <Inicio/>
     <Footer/>
     </>
 
-    
   )
 }
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,91 @@
+import '../css/Inicio.css'
+
+function Inicio(){
+    return(
+        <>
+            <div style={{backgroundImage: "url(src/IMG/olea22.jpg)", height: "500px", backgroundRepeat:"no-repeat"}}>
+                <h1>Te presentamos una muy variada lista de las obras mas populares del mundo</h1>
+                <button>Ver la coleccion</button>
+            </div>
+
+            <h1>Catálogo de Objetos</h1>
+
+        <div className='catalogo'>
+            <div className='producto'>
+                <h2>Producto 1</h2>
+                <p>Descripción del producto 1.</p>
+                <p>$100.00</p>
+            </div>
+
+            <div className='producto' >
+                <h2>Producto 2</h2>
+                <p>Descripción del producto 2.</p>
+                <p>$150.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 3</h2>
+                <p>Descripción del producto 3.</p>
+                <p>$80.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 1</h2>
+                <p>Descripción del producto 1.</p>
+                <p>$100.00</p>
+            </div>
+
+            <div className='producto' >
+                <h2>Producto 2</h2>
+                <p>Descripción del producto 2.</p>
+                <p>$150.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 3</h2>
+                <p>Descripción del producto 3.</p>
+                <p>$80.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 1</h2>
+                <p>Descripción del producto 1.</p>
+                <p>$100.00</p>
+            </div>
+
+            <div className='producto' >
+                <h2>Producto 2</h2>
+                <p>Descripción del producto 2.</p>
+                <p>$150.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 3</h2>
+                <p>Descripción del producto 3.</p>
+                <p>$80.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 1</h2>
+                <p>Descripción del producto 1.</p>
+                <p>$100.00</p>
+            </div>
+
+            <div className='producto' >
+                <h2>Producto 2</h2>
+                <p>Descripción del producto 2.</p>
+                <p>$150.00</p>
+            </div>
+
+            <div className='producto'>
+                <h2>Producto 3</h2>
+                <p>Descripción del producto 3.</p>
+                <p>$80.00</p>
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default Inicio
