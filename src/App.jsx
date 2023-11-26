2a1508a69bd07a200bc9b1551a2f6a2f9ebf1278
Victoria Torres <torresvictoriaf@outlook.com>
2023-11-26 18:12:55 -0300

    agregue imagenes y borre imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Inicio from './components/Inicio'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Inicio from "./components/Inicio";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
-    <Header/>
-
-    <Inicio/>
-    <Footer/>
+      <Header />
+      <Inicio />
+      <Footer />
     </>
-
-  )
+  );
 }
