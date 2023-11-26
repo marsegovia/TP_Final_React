ffb9e7bf556eeab143fa96b8c03cb175833f380c
Victoria Torres <torresvictoriaf@outlook.com>
2023-11-26 18:14:01 -0300

    formatee el codigo

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,91 +1,101 @@
-import '../css/Inicio.css'
+import "../css/Inicio.css";
 
-function Inicio(){
-    return(
-        <>
-            <div className='intro' style={{backgroundImage: "url(src/IMG/Red.jpg)", height: "600px", backgroundRepeat:"no-repeat"}}>
-                <h1>VIMART</h1>
-                <h3>Te presentamos una muy variada lista <br /> de las obras mas populares del mundo</h3>
-                <button className='boton'>Ver la coleccion</button>
-            </div>
+function Inicio() {
+  return (
+    <>
+      <div
+        className="intro"
+        style={{
+          backgroundImage: "url(src/IMG/Red.jpg)",
+          height: "600px",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h1>VIMART</h1>
+        <h3>
+          Te presentamos una muy variada lista <br /> de las obras mas populares
+          del mundo
+        </h3>
+        <button className="boton">Ver la coleccion</button>
+      </div>
 
-            <h2 className='texto'> Catálogo de Objetos</h2>
+      <h2 className="texto"> Catálogo de Objetos</h2>
 
-<div className='catalogo'>
-            <div className='producto'>
-                <h2>Producto 1</h2>
-                <p>Descripción del producto 1.</p>
-                <p>$100.00</p>
-            </div>
+      <div className="catalogo">
+        <div className="producto">
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1.</p>
+          <p>$100.00</p>
+        </div>
 
-            <div className='producto' >
-                <h2>Producto 2</h2>
-                <p>Descripción del producto 2.</p>
-                <p>$150.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 2</h2>
+          <p>Descripción del producto 2.</p>
+          <p>$150.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 3</h2>
-                <p>Descripción del producto 3.</p>
-                <p>$80.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 3</h2>
+          <p>Descripción del producto 3.</p>
+          <p>$80.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 1</h2>
-                <p>Descripción del producto 1.</p>
-                <p>$100.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1.</p>
+          <p>$100.00</p>
+        </div>
 
-            <div className='producto' >
-                <h2>Producto 2</h2>
-                <p>Descripción del producto 2.</p>
-                <p>$150.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 2</h2>
+          <p>Descripción del producto 2.</p>
+          <p>$150.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 3</h2>
-                <p>Descripción del producto 3.</p>
-                <p>$80.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 3</h2>
+          <p>Descripción del producto 3.</p>
+          <p>$80.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 1</h2>
-                <p>Descripción del producto 1.</p>
-                <p>$100.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1.</p>
+          <p>$100.00</p>
+        </div>
 
-            <div className='producto' >
-                <h2>Producto 2</h2>
-                <p>Descripción del producto 2.</p>
-                <p>$150.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 2</h2>
+          <p>Descripción del producto 2.</p>
+          <p>$150.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 3</h2>
-                <p>Descripción del producto 3.</p>
-                <p>$80.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 3</h2>
+          <p>Descripción del producto 3.</p>
+          <p>$80.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 1</h2>
-                <p>Descripción del producto 1.</p>
-                <p>$100.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1.</p>
+          <p>$100.00</p>
+        </div>
 
-            <div className='producto' >
-                <h2>Producto 2</h2>
-                <p>Descripción del producto 2.</p>
-                <p>$150.00</p>
-            </div>
+        <div className="producto">
+          <h2>Producto 2</h2>
+          <p>Descripción del producto 2.</p>
+          <p>$150.00</p>
+        </div>
 
-            <div className='producto'>
-                <h2>Producto 3</h2>
-                <p>Descripción del producto 3.</p>
-                <p>$80.00</p>
-            </div>
-</div>
-        </>
-    )
+        <div className="producto">
+          <h2>Producto 3</h2>
+          <p>Descripción del producto 3.</p>
+          <p>$80.00</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Inicio
+export default Inicio;
